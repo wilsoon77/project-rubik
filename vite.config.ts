50b7eecf28b754825c9e77bda368a421ba878280
Wilson7w7 <wilsoncoc5678@gmail.com>
2025-07-31 00:10:29 -0600

    Correccion y mejoras 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,28 @@
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   root: '.',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        about: './sobre-nosotros.html',
+        products: './productos.html',
+        learn: './aprende.html',
+        contact: './contacto.html'
+      }
+    }
+  },
   server: {
-    port: 3000
+    port: 3000,
+    open: true,
+    host: true
+  },
+  preview: {
+    port: 4173,
+    open: true
   }
-}
+})
