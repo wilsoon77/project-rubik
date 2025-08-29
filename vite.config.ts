642e499511000c5180d596f4d0df922192ce4b46
Wilson7w7 <wilsoncoc5678@gmail.com>
2025-08-29 14:18:18 -0600

    update vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,18 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 
 export default defineConfig({
   root: '.',
-  base: './', // Añadir esta línea para usar rutas relativas
   publicDir: 'public',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'sobre-nosotros.html'),
-        products: resolve(__dirname, 'productos.html'),
-        learn: resolve(__dirname, 'aprende.html'),
-        contact: resolve(__dirname, 'contacto.html')
-      },
-      output: {
-        entryFileNames: 'scripts/[name].js',
-        chunkFileNames: 'scripts/[name].js',
-        assetFileNames: ({name}) => {
-          if (/\.(css)$/.test(name ?? '')) {
-            return 'styles/[name][extname]';
-          }
-          return 'assets/[name][extname]';
-        }
+        main: './index.html',
+        about: './sobre-nosotros.html',
+        products: './productos.html',
+        learn: './aprende.html',
+        contact: './contacto.html'
       }
     }
   },
