13aae85859560df243cd283754cecaccead06de6
Wilson Coc <106719293+wilsoon77@users.noreply.github.com>
2025-08-29 00:53:53 -0600

    Fix Appwrite configuration error

--- scripts/config.js
+++ scripts/config.js
@@ -1,9 +1,9 @@
 //archivo de configuración - config.js
 const appwriteConfig = {
-    endpoint: import.meta.env.VITE_APPWRITE_ENDPOINT,
-    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
-    databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
-    collectionId: import.meta.env.VITE_APPWRITE_COLLECTION_ID
+    endpoint: import.meta.env.VITE_APPWRITE_ENDPOINT || 'https://cloud.appwrite.io/v1',
+    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID || '',
+    databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID || '',
+    collectionId: import.meta.env.VITE_APPWRITE_COLLECTION_ID || ''
 };
 
 // Verificación de configuración
